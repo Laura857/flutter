ed4f021552bbb85b1a3e1787d52cb5636235abad
Laura <laura@macbook-pro-de-laura.home>
2022-02-16 13:45:03 +0100

    Play music

--- lib/view/musicDetails.dart
+++ lib/view/musicDetails.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
+
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_iim/functions/firestoreHelper.dart';
 import 'package:flutter_iim/model/Morceau.dart';
 
 class musicDetails extends StatefulWidget {
@@ -15,7 +17,20 @@ class musicDetails extends StatefulWidget {
 }
 
 class musicDetailsState extends State<musicDetails> {
-  double musicTime = 0;
+  statut lecture = statut.stopped;
+  AudioPlayer audioPlayer = AudioPlayer();
+  Duration position = Duration(seconds: 0);
+  StreamSubscription? positionStream;
+  StreamSubscription? stateStream;
+  double volumeSound = 0.5;
+  Duration duree = Duration(seconds: 0);
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    configurationPlayer();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,49 +39,159 @@ class musicDetailsState extends State<musicDetails> {
       appBar: AppBar(
         title: Text(widget.morceau.title),
       ),
-      body: Container(
-        padding: EdgeInsets.all(20),
-        child: bodyPage(),
-      ),
+      body: bodyPage(),
     );
   }
 
   Widget bodyPage() {
-    return Column(children: [
-      Container(
-        height: 150,
-        width: 150,
-        decoration: BoxDecoration(
-          shape: BoxShape.rectangle,
-          image: DecorationImage(
-              image: (widget.morceau.image == null)
-                  ? NetworkImage(
-                      "https://firebasestorage.googleapis.com/v0/b/firstprojetimm.appspot.com/o/image_disponible.png?alt=media&token=809cfa6c-b1af-44e1-bd85-a12ae9ef0f39")
-                  : NetworkImage(widget.morceau.image!),
-              fit: BoxFit.fill),
+    return Column(
+      children: [
+        SizedBox(
+          height: 10,
         ),
-      ),
-      SizedBox(height: 10),
-      Text(widget.morceau.title),
-      Text(widget.morceau.author),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.fast_rewind)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.play_arrow)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.fast_forward)),
-        ],
-      ),
-      Slider(
-        min: 0.0,
-        max: 100.0,
-        value: musicTime,
-        onChanged: (value) {
-          setState(() {
-            musicTime = value;
-          });
-        },
-      )
-    ]);
+        Center(
+          child: Container(
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                image: DecorationImage(
+                    image: NetworkImage(widget.morceau.image_music!),
+                    fit: BoxFit.fill)),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(widget.morceau.title),
+        Text(widget.morceau.author),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              icon: Icon(Icons.fast_rewind),
+              onPressed: () {
+                lecture = statut.playing;
+                rewind();
+              },
+            ),
+            (lecture == statut.stopped)
+                ? IconButton(
+                    icon: Icon(Icons.play_arrow, size: 40),
+                    onPressed: () {
+                      lecture = statut.paused;
+                      play();
+                    },
+                  )
+                : IconButton(
+                    icon: Icon(
+                      Icons.pause,
+                      size: 40,
+                    ),
+                    onPressed: () {
+                      //musique en pause
+                      setState(() {
+                        lecture = statut.stopped;
+                        pause();
+                      });
+                    },
+                  ),
+            IconButton(
+              icon: Icon(Icons.fast_forward),
+              onPressed: () {
+                //musique en en avance
+                lecture = statut.playing;
+                forward();
+              },
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(position.toString().substring(2, 7)),
+            Text(duree.toString().substring(2, 7))
+          ],
+        ),
+        Slider(
+            max: (duree == null) ? 0.0 : duree.inSeconds.toDouble(),
+            min: 0.0,
+            value: position.inSeconds.toDouble(),
+            activeColor: Colors.green,
+            inactiveColor: Colors.red,
+            onChanged: (va) {
+              setState(() {
+                Duration time = Duration(seconds: va.toInt());
+                position = time;
+                audioPlayer.play(widget.morceau.song_path,
+                    position: position, volume: volumeSound);
+              });
+            }),
+      ],
+    );
+  }
+
+  Future play() async {
+    if (position > Duration(seconds: 0)) {
+      await audioPlayer.play(widget.morceau.song_path,
+          position: position, volume: volumeSound);
+    } else {
+      await audioPlayer.play(widget.morceau.song_path,
+          position: position, volume: volumeSound);
+    }
+
+    //configurationPlayer();
+  }
+
+  Future pause() async {
+    await audioPlayer.pause();
+  }
+
+  rewind() {
+    audioPlayer.pause();
+    audioPlayer.seek(Duration(seconds: 0));
+    position = new Duration(seconds: 0);
+    audioPlayer.play(widget.morceau.song_path, position: position);
+  }
+
+  forward() {
+    position = new Duration(seconds: position.inSeconds + 10);
+    audioPlayer.pause();
+    audioPlayer.play(widget.morceau.song_path, position: position);
+  }
+
+  configurationPlayer() {
+    audioPlayer.setUrl(widget.morceau.song_path);
+    positionStream = audioPlayer.onAudioPositionChanged.listen((event) {
+      setState(() {
+        position = event;
+      });
+    });
+
+    audioPlayer.onDurationChanged.listen((event) {
+      setState(() {
+        duree = event;
+      });
+    });
+    stateStream = audioPlayer.onPlayerStateChanged.listen((event) {
+      if (event == statut.playing) {
+        setState(() async {
+          duree = audioPlayer.getDuration() as Duration;
+        });
+      } else if (event == statut.stopped) {
+        setState(() {
+          lecture = statut.stopped;
+        });
+      }
+    }, onError: (message) {
+      print("erreur : $message");
+      setState(() {
+        lecture = statut.stopped;
+        position = Duration(seconds: 0);
+        duree = Duration(seconds: 0);
+      });
+    });
   }
 }
+
+enum statut { playing, stopped, paused, rewind, forward }
